98f4f623636d0605cb94a09fa3f988ecec987280
bianca <bianca.palaciosrossi@gmail.com>
2020-10-14 00:02:24 -0300

    window.registro = []; //switchery codigo var elem = document.querySelector('.js-switch'); var init = new Switchery(elem,{     color: 'green',     secondaryColor:'red',     jackColor:'white' });
    const boton = document.querySelector("#registrar-btn");
    
    boton.addEventListener('click', ()=>{
        let erroresDiv = document.querySelector("#errores-div");
        erroresDiv.innerHTML = "";
    
        let nombre = document.querySelector("#nombre-txt").value.trim();
        let planes = document.querySelector("#planes-select").value;
        let pago = document.querySelector("#pagos-select").value;
        let estado = document.querySelector("#estado-pago").checked;
    
        let errores = [];
    
        if(nombre===''){
            errores.push("Debe ingregar su nombre");
        }
        if(planes===''){
            errores.push("Debe elegir tipo de plan");
        }
        if(pago===''){
            errores.push("Debe elegir medio de pago");
        }
        if(errores.length === 0){
            let registro ={};
            registro.nombre = nombre;
            registro.planes = planes;
            registro.pago = pago;
            window.registros.push(registro);
            window.cargarTabla();
        }else {
            window.mostrarErrores(errores);
    
        }
    
    });

---
+++ js/index.js
@@ -0,0 +1,45 @@
+window.registro = [];
+//switchery codigo
+var elem = document.querySelector('.js-switch');
+var init = new Switchery(elem,{
+    color: 'green',
+    secondaryColor:'red',
+    jackColor:'white'
+});
+
+const boton = document.querySelector("#registrar-btn");
+
+boton.addEventListener('click', ()=>{
+    let erroresDiv = document.querySelector("#errores-div");
+    erroresDiv.innerHTML = "";
+
+    let nombre = document.querySelector("#nombre-txt").value.trim();
+    let planes = document.querySelector("#planes-select").value;
+    let pago = document.querySelector("#pagos-select").value;
+    let estado = document.querySelector("#estado-pago").checked;
+
+    let errores = [];
+
+    if(nombre===''){
+        errores.push("Debe ingregar su nombre");
+    }
+    if(planes===''){
+        errores.push("Debe elegir tipo de plan");
+    }
+    if(pago===''){
+        errores.push("Debe elegir medio de pago");
+    }
+    if(errores.length === 0){
+        let registro ={};
+        registro.nombre = nombre;
+        registro.planes = planes;
+        registro.pago = pago;
+        window.registros.push(registro);
+        window.cargarTabla();
+    }else {
+        window.mostrarErrores(errores);
+
+    }
+
+
+});
